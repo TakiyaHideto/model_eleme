359267b053f63a2d52791b3e8b996a5252dfe521
张蔚 <zhangwei@zhangweideMacBook-Pro.local>
2016-11-05 13:55:21 +0800

    修正注释解除后indent没有对齐的问题

--- hackthon_eleme.py
+++ hackthon_eleme.py
@@ -81,137 +81,137 @@ if __name__ == '__main__':
 
     job.joinData()
 
-     # traing clk
-     splitData(data_file_path=output_file_clk,
-               train_file_path=train_raw_path,
-               test_file_path=test_raw_path)
-     feature_engineering(input_file=train_raw_path,
-                         output_file=train_file_path + 'clk',
-                         feat_map_file=feat_map_file)
-     feature_engineering(input_file=test_raw_path,
-                         output_file=test_file_path + 'clk',
-                         feat_map_file=feat_map_file)
-     trainTest(configuration='configuration_clk')
+    # traing clk
+    splitData(data_file_path=output_file_clk,
+              train_file_path=train_raw_path,
+              test_file_path=test_raw_path)
+    feature_engineering(input_file=train_raw_path,
+                        output_file=train_file_path + 'clk',
+                        feat_map_file=feat_map_file)
+    feature_engineering(input_file=test_raw_path,
+                        output_file=test_file_path + 'clk',
+                        feat_map_file=feat_map_file)
+    trainTest(configuration='configuration_clk')
 
-     # traing buy
-     splitData(data_file_path=output_file_buy,
-               train_file_path=train_raw_path,
-               test_file_path=test_raw_path)
-     feature_engineering(input_file=train_raw_path,
-                         output_file=train_file_path + 'buy',
-                         feat_map_file=feat_map_file)
-     feature_engineering(input_file=test_raw_path,
-                         output_file=test_file_path + 'buy',
-                         feat_map_file=feat_map_file)
-     trainTest(configuration='configuration_buy')
+    # traing buy
+    splitData(data_file_path=output_file_buy,
+              train_file_path=train_raw_path,
+              test_file_path=test_raw_path)
+    feature_engineering(input_file=train_raw_path,
+                        output_file=train_file_path + 'buy',
+                        feat_map_file=feat_map_file)
+    feature_engineering(input_file=test_raw_path,
+                        output_file=test_file_path + 'buy',
+                        feat_map_file=feat_map_file)
+    trainTest(configuration='configuration_buy')
 
-     # predict nxt
-     feature_engineering(output_file_nxt, output_file_nxt_feat_eng, feat_map_file)
-     predict(configuration='configuration_clk',
-             model_in='clk_mdl.model',
-             prediction_file=output_file_nxt_feat_eng)
-     predict(configuration='configuration_buy',
-             model_in='buy_mdl.model',
-             prediction_file=output_file_nxt_feat_eng)
-
-
- ################################################################################################
-     clk_probabilty = []
-     with open('pred_clk.txt', 'r') as fi:
-         for line in fi:
-             clk_probabilty.append(line.rstrip())
-
-     buy_probabilty = []
-     with open('pred_buy.txt', 'r') as fi:
-         for line in fi:
-             buy_probabilty.append(line.rstrip())
+    # predict nxt
+    feature_engineering(output_file_nxt, output_file_nxt_feat_eng, feat_map_file)
+    predict(configuration='configuration_clk',
+            model_in='clk_mdl.model',
+            prediction_file=output_file_nxt_feat_eng)
+    predict(configuration='configuration_buy',
+            model_in='buy_mdl.model',
+            prediction_file=output_file_nxt_feat_eng)
 
 
-     clk_usr_pre_dict = {}
-     with open(output_file_nxt, 'r') as fi:
-         i = 0
-         for line in fi:
-             elements = line.rstrip().split('\t')
-             user_id = elements[13]
-             if clk_usr_pre_dict.has_key(user_id):
-                 clk_usr_pre_dict[user_id].append(float(clk_probabilty[i]))
-             else:
-                 clk_usr_pre_dict[user_id] = []
-                 clk_usr_pre_dict[user_id].append(float(clk_probabilty[i]))
-             clk_probabilty[i] = "{0}${1}".format(clk_probabilty[i],user_id)
-             i += 1
+ ###############################################################################################
+    clk_probabilty = []
+    with open('pred_clk.txt', 'r') as fi:
+        for line in fi:
+            clk_probabilty.append(line.rstrip())
 
-     buy_usr_pre_dict = {}
-     with open(output_file_nxt, 'r') as fi:
-         i = 0
-         for line in fi:
-             elements = line.rstrip().split('\t')
-             user_id = elements[13]
-             if buy_usr_pre_dict.has_key(user_id):
-                 buy_usr_pre_dict[user_id].append(float(buy_probabilty[i]))
-             else:
-                 buy_usr_pre_dict[user_id] = []
-                 buy_usr_pre_dict[user_id].append(float(buy_probabilty[i]))
-             buy_probabilty[i] = "{0}${1}".format(buy_probabilty[i], user_id)
-             i += 1
-
-     for key in clk_usr_pre_dict.keys():
-         clk_usr_pre_dict[key] = sorted(clk_usr_pre_dict[key], reverse=True)
-         buy_usr_pre_dict[key] = sorted(buy_usr_pre_dict[key], reverse=True)
+    buy_probabilty = []
+    with open('pred_buy.txt', 'r') as fi:
+        for line in fi:
+            buy_probabilty.append(line.rstrip())
 
 
-     clk_pre = []
-     raw_data = []
-     for i in range(0, len(clk_probabilty)):
-         probabilty = float(clk_probabilty[i].split('$')[0])
-         user_id = clk_probabilty[i].split('$')[1]
-         threshold = float(clk_usr_pre_dict[user_id][int(len(clk_usr_pre_dict[user_id])*USER_CLK_THRESHOLD)])
-         if probabilty > threshold:
-             clk_pre.append('1')
-         else:
-             clk_pre.append('0')
-     # with open('pred_clk.txt', 'r') as fi:
-     #     for line in fi:
-     #         raw_data.append(float(line.rstrip()))
-     #     threshold = sorted(raw_data, reverse=True)[int(len(raw_data) * 0.05 + 0.5)]
-     #     for value in raw_data:
-     #         if value < threshold:
-     #             prob = '0'
-     #         else:
-     #             prob = '1'
-     #         clk_pre.append(prob)
+    clk_usr_pre_dict = {}
+    with open(output_file_nxt, 'r') as fi:
+        i = 0
+        for line in fi:
+            elements = line.rstrip().split('\t')
+            user_id = elements[13]
+            if clk_usr_pre_dict.has_key(user_id):
+                clk_usr_pre_dict[user_id].append(float(clk_probabilty[i]))
+            else:
+                clk_usr_pre_dict[user_id] = []
+                clk_usr_pre_dict[user_id].append(float(clk_probabilty[i]))
+            clk_probabilty[i] = "{0}${1}".format(clk_probabilty[i],user_id)
+            i += 1
 
-     buy_pre = []
-     raw_data = []
-     for i in range(0, len(buy_probabilty)):
-         probabilty = float(buy_probabilty[i].split('$')[0])
-         user_id = buy_probabilty[i].split('$')[1]
-         threshold = float(buy_usr_pre_dict[user_id][int(len(buy_usr_pre_dict[user_id]) * USER_BUY_THRESHOLD)])
-         if probabilty > threshold:
-             buy_pre.append('1')
-         else:
-             buy_pre.append('0')
-     # with open('pred_buy.txt', 'r') as fi:
-     #     for line in fi:
-     #         raw_data.append(float(line.rstrip()))
-     #     threshold = sorted(raw_data, reverse=True)[int(len(raw_data) * 0.001 + 0.5)]
-     #     for value in raw_data:
-     #         if value < threshold:
-     #             prob = '0'
-     #         else:
-     #             prob = '1'
-     #         buy_pre.append(prob)
+    buy_usr_pre_dict = {}
+    with open(output_file_nxt, 'r') as fi:
+        i = 0
+        for line in fi:
+            elements = line.rstrip().split('\t')
+            user_id = elements[13]
+            if buy_usr_pre_dict.has_key(user_id):
+                buy_usr_pre_dict[user_id].append(float(buy_probabilty[i]))
+            else:
+                buy_usr_pre_dict[user_id] = []
+                buy_usr_pre_dict[user_id].append(float(buy_probabilty[i]))
+            buy_probabilty[i] = "{0}${1}".format(buy_probabilty[i], user_id)
+            i += 1
+
+    for key in clk_usr_pre_dict.keys():
+        clk_usr_pre_dict[key] = sorted(clk_usr_pre_dict[key], reverse=True)
+        buy_usr_pre_dict[key] = sorted(buy_usr_pre_dict[key], reverse=True)
 
 
-     with open(file_nxt_eco_info_path, 'r') as fi:
-         i = 0
-         j = 0
-         for line in fi:
-             log_id = line.rstrip().split('\t')[0]
-             if j == 0:
-                 j += 1
-                 continue
-             print >>sys.stdout, log_id + '\t' + clk_pre[i] + '\t' + buy_pre[i]
-             i += 1
+    clk_pre = []
+    raw_data = []
+    for i in range(0, len(clk_probabilty)):
+        probabilty = float(clk_probabilty[i].split('$')[0])
+        user_id = clk_probabilty[i].split('$')[1]
+        threshold = float(clk_usr_pre_dict[user_id][int(len(clk_usr_pre_dict[user_id])*USER_CLK_THRESHOLD)])
+        if probabilty > threshold:
+            clk_pre.append('1')
+        else:
+            clk_pre.append('0')
+    # with open('pred_clk.txt', 'r') as fi:
+    #     for line in fi:
+    #         raw_data.append(float(line.rstrip()))
+    #     threshold = sorted(raw_data, reverse=True)[int(len(raw_data) * 0.05 + 0.5)]
+    #     for value in raw_data:
+    #         if value < threshold:
+    #             prob = '0'
+    #         else:
+    #             prob = '1'
+    #         clk_pre.append(prob)
 
-     print >>sys.stderr, len(clk_usr_pre_dict.keys())
+    buy_pre = []
+    raw_data = []
+    for i in range(0, len(buy_probabilty)):
+        probabilty = float(buy_probabilty[i].split('$')[0])
+        user_id = buy_probabilty[i].split('$')[1]
+        threshold = float(buy_usr_pre_dict[user_id][int(len(buy_usr_pre_dict[user_id]) * USER_BUY_THRESHOLD)])
+        if probabilty > threshold:
+            buy_pre.append('1')
+        else:
+            buy_pre.append('0')
+    # with open('pred_buy.txt', 'r') as fi:
+    #     for line in fi:
+    #         raw_data.append(float(line.rstrip()))
+    #     threshold = sorted(raw_data, reverse=True)[int(len(raw_data) * 0.001 + 0.5)]
+    #     for value in raw_data:
+    #         if value < threshold:
+    #             prob = '0'
+    #         else:
+    #             prob = '1'
+    #         buy_pre.append(prob)
+
+
+    with open(file_nxt_eco_info_path, 'r') as fi:
+        i = 0
+        j = 0
+        for line in fi:
+            log_id = line.rstrip().split('\t')[0]
+            if j == 0:
+                j += 1
+                continue
+            print >>sys.stdout, log_id + '\t' + clk_pre[i] + '\t' + buy_pre[i]
+            i += 1
+
+    print >>sys.stderr, len(clk_usr_pre_dict.keys())
